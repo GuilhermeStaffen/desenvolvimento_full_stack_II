8fce98b0e09473bc483dedc679172c814ad7b9fb
Guilherme Staffen <guilhermestaffen@gmail.com>
2025-09-04 08:48:22 -0300

    Corrigindo dupla sincronização de BD, que ocasionava erros ao iniciar o projeto

--- models/index.js
+++ models/index.js
@@ -1,13 +1,15 @@
 const sequelize = require('../config/database');
 const User = require('./User');
 const Product = require('./Product');
+const Cart = require('./Cart');
 
 async function syncDatabase() {
   try {
-    await sequelize.sync(); 
+    await sequelize.sync({ alter: true });
+    console.log('Banco sincronizado com sucesso!');
   } catch (error) {
     console.error('Erro ao sincronizar DB:', error);
   }
 }
 
-module.exports = { sequelize, User, Product, syncDatabase }
+module.exports = { sequelize, User, Product, Cart, syncDatabase };
