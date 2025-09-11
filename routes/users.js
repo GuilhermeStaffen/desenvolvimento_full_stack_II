188edc5c8d6ce764dc2d7b2ce1b922927a583c00
Guilherme Staffen <guilhermestaffen@gmail.com>
2025-09-11 08:43:55 -0300

    Ajustando swagger

--- routes/users.js
+++ routes/users.js
@@ -13,6 +13,67 @@ const authorizeRoles = require('../middlewares/roleMiddleware');
 
 /**
  * @swagger
+ * components:
+ *   schemas:
+ *     Address:
+ *       type: object
+ *       required:
+ *         - street
+ *         - number
+ *         - city
+ *         - state
+ *         - zipcode
+ *         - country
+ *       properties:
+ *         street:
+ *           type: string
+ *           example: Rua das Flores
+ *         number:
+ *           type: string
+ *           example: 123
+ *         city:
+ *           type: string
+ *           example: São Paulo
+ *         state:
+ *           type: string
+ *           example: SP
+ *         zipcode:
+ *           type: string
+ *           example: 01000-000
+ *         country:
+ *           type: string
+ *           example: Brasil
+ *
+ *     User:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *           example: 1
+ *         name:
+ *           type: string
+ *           example: João Silva
+ *         email:
+ *           type: string
+ *           example: joao@email.com
+ *         userType:
+ *           type: string
+ *           example: customer
+ *         phone:
+ *           type: string
+ *           example: "+55 11 99999-8888"
+ *         address:
+ *           $ref: '#/components/schemas/Address'
+ *         createdAt:
+ *           type: string
+ *           format: date-time
+ *         updatedAt:
+ *           type: string
+ *           format: date-time
+ */
+
+/**
+ * @swagger
  * /users:
  *   post:
  *     summary: Criar um novo usuário
@@ -27,6 +88,7 @@ const authorizeRoles = require('../middlewares/roleMiddleware');
  *               - name
  *               - email
  *               - password
+ *               - address
  *             properties:
  *               name:
  *                 type: string
@@ -37,9 +99,18 @@ const authorizeRoles = require('../middlewares/roleMiddleware');
  *               password:
  *                 type: string
  *                 example: 123456
+ *               phone:
+ *                 type: string
+ *                 example: "+55 11 99999-8888"
+ *               address:
+ *                 $ref: '#/components/schemas/Address'
  *     responses:
  *       201:
  *         description: Usuário criado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
  *       400:
  *         description: Dados inválidos
  *       500:
@@ -53,6 +124,12 @@ const authorizeRoles = require('../middlewares/roleMiddleware');
  *     responses:
  *       200:
  *         description: Lista de usuários
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
  *       403:
  *         description: Acesso negado
  *       500:
@@ -77,6 +154,10 @@ const authorizeRoles = require('../middlewares/roleMiddleware');
  *     responses:
  *       200:
  *         description: Usuário encontrado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
  *       403:
  *         description: Acesso negado
  *       404:
@@ -111,9 +192,18 @@ const authorizeRoles = require('../middlewares/roleMiddleware');
  *               password:
  *                 type: string
  *                 example: novaSenha123
+ *               phone:
+ *                 type: string
+ *                 example: "+55 11 98888-7777"
+ *               address:
+ *                 $ref: '#/components/schemas/Address'
  *     responses:
  *       200:
  *         description: Usuário atualizado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
  *       403:
  *         description: Acesso negado
  *       404:
@@ -141,7 +231,6 @@ const authorizeRoles = require('../middlewares/roleMiddleware');
  *       500:
  *         description: Erro interno
  */
-
 
 router.post('/', userController.create);
 router.get('/', authMiddleware, authorizeRoles('admin'),userController.list);
